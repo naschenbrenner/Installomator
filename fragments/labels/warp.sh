7f042794378a3181672fb76726d06bbf6ec34cc9
naschenbrenner <60478794+naschenbrenner@users.noreply.github.com>
2025-03-29 23:16:35 -0500

    Create warp.sh

---
+++ fragments/labels/warp.sh
@@ -0,0 +1,8 @@
+warp)
+    name="Warp"
+    type="dmg"
+    downloadURL="https://app.warp.dev/download?package=dmg"
+    appNewVersion=$(curl -fs "https://app.warp.dev/download?package=dmg" | grep -o 'v[0-9]\.[0-9]\{4\}\.[0-9]\{2\}\.[0-9]\{2\}\.[0-9]\{2\}\.[0-9]\{2\}\.stable_[0-9]\{2\}')
+    expectedTeamID="2BBY89MBSN"
+    versionKey="WarpVersion"
+    ;;
